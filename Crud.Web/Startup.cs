83bbf2a0a09f8f42e121c3fbb0e597f837a256e9
John Darragh <darragh@entrotech.net>
2016-06-14 15:53:15 -0700

    Initial commit

---
+++ Crud.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Crud.Web.Startup))]
+namespace Crud.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
